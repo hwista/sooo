9738634cb10d38fc20fa482c4b43ed933aa95e2b
Hwista <duolctroo@hotmail.com>
2026-01-23 16:54:22 +0900

    chore(storybook): simplify to docs-only mode
    - Remove addon-interactions, addon-links, addon-a11y
    - Keep only addon-essentials for documentation generation
    - Remove actions config from preview.ts
    - Static docs will be hosted on DMS

--- apps/web/pms/.storybook/main.ts
+++ apps/web/pms/.storybook/main.ts
@@ -4,9 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
